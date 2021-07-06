584ce2632fb311423dd4063ecc037b7426ffbe1b
ISs25u <11806309+ISs25u@users.noreply.github.com>
2021-07-06 16:00:27 +0200

    Always day (temporary?)

--- register.lua
+++ register.lua
@@ -61,6 +61,13 @@ minetest.register_on_joinplayer(function(player)
         minetest.chat_send_player(name, S('Cannot create @1', path))
     end
 
+    -- TODO: TEMP fix ?
+    player:override_day_night_ratio(1)
+    player:set_stars({visible = false})
+    player:set_sun({visible = false})
+    player:set_moon({visible = false})
+    player:set_clouds({density = 0})
+
     -- player:get_meta():set_int('codeblock:last_index', 0)
 
 end)
