a8edb512297285cb012def9baf3d5bbd33fcb37c
ISs25u <11806309+ISs25u@users.noreply.github.com>
2021-11-30 18:51:09 +0100

    - Locale update

--- gen_locale_en.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sed 's|\([^@]\)=|\1·|gm' locale/codeblock.en.tr | awk 'BEGIN {FS="·"} {print $2}' >locale/en.tr
-sed 's|\([^@]\)=|\1·|gm' locale/codeblock.fr.tr | awk 'BEGIN {FS="·"} {print $2}' >locale/fr.tr
