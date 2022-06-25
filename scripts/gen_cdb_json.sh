567531d17cac94313205ae4fd47a70bf371f8189
gigaturbo <gigaturbo@protonmail.com>
2022-06-25 16:41:24 +0200

    Updace cdb

--- scripts/gen_cdb_json.sh
+++ scripts/gen_cdb_json.sh
@@ -12,9 +12,8 @@ printf \
         "education",
         "tools"
     ],
-    "content_warnings": [],
     "license": "GPL-3.0-only",
     "media_license": "GPL-3.0-only",
     "repo": "https://github.com/gigaturbo/codeblock",
-    "issue_tracker": "https://github.com/gigaturbo/codeblock/issues",
+    "issue_tracker": "https://github.com/gigaturbo/codeblock/issues"
 }' "$(perl -0777 -pe 's|\n|\\n|gs' README.md)" > .cdb.json
